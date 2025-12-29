9b8ca6a3de849e6ffdec562b88625d43c264e565
tcooper0528 <tcooper0528@gmail.com>
2025-12-29 13:41:01 -0600

    app.js

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const resetText = document.getElementById('resetText');
 resetBtn.addEventListener('click', () => {
 resetText.classList.toggle('hidden');
 });
+
+const verses = [
+"John 3:16",
+"John 14:6",
+"Ephesians 2:8–9",
+"Matthew 6:14",
+"Mark 12:31",
+"Isaiah 41:10",
+"Romans 12:2",
+"Micah 6:8",
+"James 1:12",
+"Philippians 4:4"
+];
+
+
+const today = new Date().getDate();
+const verse = verses[today % verses.length];
+console.log("Today's verse:", verse);
