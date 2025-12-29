41a01a0113d99341281ab817a96f72939b39e47d
tcooper0528 <tcooper0528@gmail.com>
2025-12-29 12:00:07 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const resetBtn = document.getElementById('resetBtn');
+const resetText = document.getElementById('resetText');
+
+
+resetBtn.addEventListener('click', () => {
+resetText.classList.toggle('hidden');
+});
